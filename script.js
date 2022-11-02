369426767630088d8629c1fdda91447758cfdaa7
holoflash <mikepalacemusic@gmail.com>
2022-11-02 09:05:38 +0100

    Improve mobile U.I. scaling

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ const keysound = new Audio("resources/key.mp3");
 const button = document.querySelectorAll("button");
 
 function sound(){
-    keysound.currentTime = 0.04;
+    keysound.currentTime = 0.0;
     keysound.play();
 }
 
-button.forEach(button => button.addEventListener("click", sound))
+button.forEach(button => button.addEventListener("touchstart", sound));
+button.forEach(button => button.addEventListener("touchend", tapOrClick, false));
+button.forEach(button => button.addEventListener("mousedown", sound));
+
+
+function tapOrClick(event) {
+    event.preventDefault();
+    return false;
+}
