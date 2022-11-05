ab899a31d786a0922a4696ef6014c105995d3f0a
holoflash <mikepalacemusic@gmail.com>
2022-11-05 09:30:55 +0100

    Get and display second operand, update sounds

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Sound effects
 
-const keysound = new Audio("resources/key.mp3");
+const keysound = new Audio("resources/KeyR.wav");
+const alertsound = new Audio("resources/Alertsound.wav");
 const allButtons = document.querySelectorAll("button");
 
 function sound() {
@@ -30,9 +31,13 @@ clear.addEventListener("click", clearDisplay);
 del.addEventListener("click", remove);
 
 numbers.forEach(numbers => numbers.addEventListener("click",userInput));
+numbers.forEach(numbers => numbers.addEventListener("touchstart",userInput));
 numbers.forEach(numbers => numbers.addEventListener("click",noDotStart));
+numbers.forEach(numbers => numbers.addEventListener("touchstart",noDotStart));
 numbers.forEach(numbers => numbers.addEventListener("click",onlyOneDot));
+numbers.forEach(numbers => numbers.addEventListener("touchstart",onlyOneDot));
 operate.forEach(operate => operate.addEventListener("click",operator));
+operate.forEach(operate => operate.addEventListener("touchstart",operator));
 
 
 
@@ -41,6 +46,7 @@ const calculator = {
     firstOperand: "",
     waitingForSecondOperand: false,
     operator: "",
+    secondOperand: "",
   };
 
 display.innerHTML = calculator.displayValue;
@@ -50,14 +56,24 @@ function userInput(){
     calculator.firstOperand = calculator.firstOperand.concat(`${this.getAttribute("number")}`);
     calculator.displayValue = calculator.firstOperand;
     updateDisplay();
+    if(calculator.waitingForSecondOperand==true){
+        calculator.displayValue ="0"
+        calculator.secondOperand = calculator.secondOperand.concat(`${this.getAttribute("number")}`);
+        calculator.displayValue = calculator.secondOperand;
+        updateDisplay();
+        return
+    }
 }
 
 
 function noDotStart(){
     const regex = /^\./g;
-    let dotStart = regex.test(calculator.firstOperand);
+    let dotStart = regex.test(calculator.firstOperand)
     if (dotStart === true){
         remove();
+        alertsound.currentTime = 0.0;
+        alertsound.play();
+        alertsound.volume  = 0.5;
     }
 }
 
@@ -65,19 +81,20 @@ function onlyOneDot(){
     const regex = /(\..*){2,}/;
     let dotStart = regex.test(calculator.firstOperand);
     if (dotStart === true){
+        alertsound.currentTime = 0.0;
+        alertsound.play();
+        alertsound.volume  = 0.5;
         remove();
     }
 }
 
-
 function operator(){
-    const operatorChoice = this.getAttribute("name");
-    console.log(operatorChoice);
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = this.getAttribute("name");
+    if(this.getAttribute("name")==="="){
+        decision();
+    }
 }
-
-
-
-
 
 function clearDisplay(){
     if (calculator.displayValue ==="0")
@@ -96,7 +113,6 @@ function remove(){
      if(calculator.firstOperand.length === 0){
         clearDisplay();
      }
-     
 }
 
 function updateDisplay(){
@@ -104,33 +120,11 @@ function updateDisplay(){
 }
 
 
-
-
-
-// function updateDisplay() {
-//     arr.push(userNum);
-//     for (var i = 0; i < arr.length; i++) {
-//     }
-//     display.innerHTML = (arr.join(''));
-    
-//     userNum = arr;
-// }
-
-
-
-// function userInput() {
-//     userNum = this.getAttribute("name");
-//     updateDisplay();
-//     if (arr.length >= 14) {
-//         numberButtons.forEach(numberButtons => numberButtons.removeEventListener("mousedown", userInput));
-//         numberButtons.forEach(numberButtons => numberButtons.removeEventListener("touchstart", userInput));
-//     }
-
-
-
-
-
-
+function decision(a,b){
+    a = calculator.firstOperand;
+    b = calculator.secondOperand;
+    alert(parseInt(a) + parseInt(b));
+}
 
 
 
