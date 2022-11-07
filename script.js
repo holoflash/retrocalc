d040dfdca849ab72c0a4a00d7995faf9ad7d90c1
holoflash <mikepalacemusic@gmail.com>
2022-11-07 10:41:07 +0100

    Get the calculator to work correctly

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 //Sound effects
-
 const keysound = new Audio("resources/KeyR.wav");
 const alertsound = new Audio("resources/Alertsound.wav");
 const allButtons = document.querySelectorAll("button");
+const number = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".operator");
+const display = document.querySelector("h1");
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+const del = document.querySelector(".delete");
+
 
 function sound() {
     keysound.currentTime = 0.0;
@@ -18,125 +24,102 @@ allButtons.forEach(allButtons => allButtons.addEventListener("touchstart", sound
 allButtons.forEach(allButtons => allButtons.addEventListener("touchend", tapOrClick, false));
 allButtons.forEach(allButtons => allButtons.addEventListener("mousedown", sound));
 
-
-
-const operate = document.querySelectorAll(".operator");
-const display = document.querySelector("h1");
-const clear = document.querySelector(".clear");
-const del = document.querySelector(".delete");
-const numbers = document.querySelectorAll(".number");
-
-
-clear.addEventListener("click", clearDisplay);
-del.addEventListener("click", remove);
-
-numbers.forEach(numbers => numbers.addEventListener("click",userInput));
-numbers.forEach(numbers => numbers.addEventListener("touchstart",userInput));
-numbers.forEach(numbers => numbers.addEventListener("click",noDotStart));
-numbers.forEach(numbers => numbers.addEventListener("touchstart",noDotStart));
-numbers.forEach(numbers => numbers.addEventListener("click",onlyOneDot));
-numbers.forEach(numbers => numbers.addEventListener("touchstart",onlyOneDot));
-operate.forEach(operate => operate.addEventListener("click",operator));
-operate.forEach(operate => operate.addEventListener("touchstart",operator));
+number.forEach(number => number.addEventListener("mousedown", userNumberInput));
+operator.forEach(operator => operator.addEventListener("mousedown", userOperatorInput));
+equals.addEventListener("mousedown", calculate);
+clear.addEventListener("click", clearMemory);
+del.addEventListener("click", erase);
 
 
 
-const calculator = {
-    displayValue: "0",
-    firstOperand: "",
-    waitingForSecondOperand: false,
-    operator: "",
-    secondOperand: "",
-  };
+const calc = {
+    x: "",
+    y: "",
+    op:"",
+}
 
-display.innerHTML = calculator.displayValue;
-  
+function userNumberInput(){
+   calc.x = calc.x.concat(`${this.getAttribute("number")}`);
+   display.textContent = calc.x;
+}
 
-function userInput(){
-    calculator.firstOperand = calculator.firstOperand.concat(`${this.getAttribute("number")}`);
-    calculator.displayValue = calculator.firstOperand;
-    updateDisplay();
-    if(calculator.waitingForSecondOperand==true){
-        calculator.displayValue ="0"
-        calculator.secondOperand = calculator.secondOperand.concat(`${this.getAttribute("number")}`);
-        calculator.displayValue = calculator.secondOperand;
-        updateDisplay();
-        return
+function userOperatorInput(){
+    calc.y = calc.x;
+    calc.x = "";
+    calc.op = this.getAttribute("name");
+    if (calc.op =="="){
+        calculate()
     }
 }
 
-
-function noDotStart(){
-    const regex = /^\./g;
-    let dotStart = regex.test(calculator.firstOperand)
-    if (dotStart === true){
-        remove();
-        alertsound.currentTime = 0.0;
-        alertsound.play();
-        alertsound.volume  = 0.5;
+function calculate(){
+    if(calc.op =="+"){
+        calc.x = parseInt(calc.y) + parseInt(calc.x);
     }
-}
-
-function onlyOneDot(){
-    const regex = /(\..*){2,}/;
-    let dotStart = regex.test(calculator.firstOperand);
-    if (dotStart === true){
-        alertsound.currentTime = 0.0;
-        alertsound.play();
-        alertsound.volume  = 0.5;
-        remove();
+    else if(calc.op =="-"){
+        calc.x = parseInt(calc.y) - parseInt(calc.x);
     }
-}
-
-function operator(){
-    calculator.waitingForSecondOperand = true;
-    calculator.operator = this.getAttribute("name");
-    if(this.getAttribute("name")==="="){
-        decision();
+    else if(calc.op =="*"){
+        calc.x = parseInt(calc.y) * parseInt(calc.x);
     }
+    else if(calc.op =="/"){
+        calc.x = parseInt(calc.y) / parseInt(calc.x);
+    }
+    calc.op = "";
+        calc.y = "";
+        display.textContent = calc.x;
 }
 
-function clearDisplay(){
-    if (calculator.displayValue ==="0")
-    return
-    calculator.firstOperand = "";
-    calculator.displayValue = "0"
-    updateDisplay();
+let zeroDisplay = display.textContent;
+
+function clearMemory(){
+    calc.x = "";
+    calc.y = "";
+    calc.op = "";
+    display.textContent = zeroDisplay; 
 }
 
-function remove(){
-    if (calculator.displayValue ==="0")
-     return
-    calculator.firstOperand = calculator.firstOperand.slice(0, -1);
-    calculator.displayValue = calculator.firstOperand;
-    updateDisplay();
-     if(calculator.firstOperand.length === 0){
-        clearDisplay();
+function erase(){
+    calc.x = calc.x.slice(0, -1);
+     display.textContent = calc.x;
+     if (calc.x===""){
+        display.textContent = zeroDisplay;
      }
+
 }
 
-function updateDisplay(){
-    display.innerHTML = calculator.displayValue;    
-}
+// function noDotStart(){
+//     const regex = /^\./g;
+//     let dotStart = regex.test(calculator.firstOperand)
+//     if (dotStart === true){
+//         remove();
+//         alertsound.currentTime = 0.0;
+//         alertsound.play();
+//         alertsound.volume  = 0.5;
+//     }
+// }
+
+// function onlyOneDot(){
+//     const regex = /(\..*){2,}/;
+//     let dotStart = regex.test(calculator.firstOperand);
+//     if (dotStart === true){
+//         alertsound.currentTime = 0.0;
+//         alertsound.play();
+//         alertsound.volume  = 0.5;
+//         remove();
+//     }
+// }
 
 
-function decision(a,b){
-    a = calculator.firstOperand;
-    b = calculator.secondOperand;
-    alert(parseInt(a) + parseInt(b));
-}
 
-
-
-
-// JS keycodes
-// 0	48
-// 1	49
-// 2	50
-// 3	51
-// 4	52
-// 5	53
-// 6	54
-// 7	55
-// 8	56
-// 9	57
+// // JS keycodes
+// // 0	48
+// // 1	49
+// // 2	50
+// // 3	51
+// // 4	52
+// // 5	53
+// // 6	54
+// // 7	55
+// // 8	56
+// // 9	57
