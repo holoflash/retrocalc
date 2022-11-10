dc20679be902f16ae10a9aac8316fc2473e45dbb
holoflash <mikepalacemusic@gmail.com>
2022-11-10 10:35:40 +0100

    Rewrite calcualtor code and squash all bugs

--- script.js
+++ script.js
@@ -3,15 +3,8 @@ const operator = document.querySelectorAll(".operator");
 const display = document.querySelector("h1");
 const clear = document.querySelector(".clear");
 const del = document.querySelector(".delete");
+const equal = document.getElementById("Enter");
 
-number.forEach(number => number.addEventListener("click", userNumberInput));
-number.forEach(number => number.addEventListener("click", dotManager));
-operator.forEach(operator => operator.addEventListener("click", userOperatorInput));
-clear.addEventListener("click", clearMemory);
-del.addEventListener("click", erase);
-
-
-//calculator memory
 const calc = {
     x: "",
     y: "",
@@ -19,30 +12,39 @@ const calc = {
     sum: "",
 }
 
-//number buttons
+number.forEach(number => number.addEventListener("click", userNumberInput));
+number.forEach(number => number.addEventListener("click", dotManager));
+operator.forEach(operator => operator.addEventListener("click", userOperatorInput));
+clear.addEventListener("click", clearMemory);
+del.addEventListener("click", erase);
+equal.addEventListener("click", calculate);
+window.addEventListener("keydown", keyboardInput);
+window.addEventListener("keydown", dotManager);
+
 function userNumberInput() {
-    calc.x = calc.x.concat(`${this.getAttribute("number")}`);
-    if (calc.x.length > 15) {
-        //shake
-        erase();
+    if (calc.x !== "" && calc.op !== "") {
+        calc.y = calc.y.concat(`${this.getAttribute("number")}`);
+        display.textContent = calc.y;
+    }
+    if (calc.y === "" && calc.op === "") {
+        calc.x = calc.x.concat(`${this.getAttribute("number")}`);
+        display.textContent = calc.x;
+    }
+    if (calc.x.length > 15 || calc.x.length > 15) {
+        blink();
+        setTimeout(blink, 10);
         return
     }
-    display.textContent = calc.x;
-    // debug
-    // console.log("op number section: " + calc.op)
-    // console.log("x: " + calc.x)
-    // console.log("y: " + calc.y)
-    // console.log("sum: " + calc.sum)
 }
 
-//starting with a dot adds a decimal point
-//only one dot is allowed
 function dotManager() {
     const regexDotStart = /^\./g;
     let dotStart = regexDotStart.test(calc.x);
     if (dotStart === true) {
         display.innerText = "0."
         calc.x = "0."
+        blink();
+        setTimeout(blink, 10);
         return
     }
     const regexMultiDot = /(\..*){2,}/;
@@ -52,55 +54,50 @@ function dotManager() {
     }
 }
 
-//operator buttons
 function userOperatorInput() {
-    if (calc.op === "=") {
-        calc.x = calc.sum;
+    if (calc.x !== "" && calc.op === "") {
+        calc.op = this.getAttribute("name");
     }
-    if (calc.x === "" && calc.op !== "=") {
+    if (calc.x !== "" && calc.y !== "" & calc.op !== "") {
+        calc.op = this.getAttribute("name");
+        calc.y = "";
+    }
+    if(calc.x ===""){
+        blink();
+        setTimeout(blink, 10);
         return
-    }
-    if (calc.op !== "") {
-        calculate();
-    }
-    calc.y = calc.x;
-    calc.x = "";
-    calc.op = this.getAttribute("name");
-    if (calc.y !== "" || calc.x !== "") {
-        calculate();
     }
 }
 
-
-//math operations
 function calculate() {
-    if (calc.op === "" || calc.x === "") {
+    if (calc.op === "" || calc.y ==="") {
+        blink();
+        setTimeout(blink, 10);
         return
     }
     if (calc.op === "+") {
-        calc.sum = parseFloat(calc.y) + parseFloat(calc.x);
+        calc.sum = parseFloat(calc.x) + parseFloat(calc.y);
     }
     else if (calc.op === "-") {
-        calc.sum = parseFloat(calc.y) - parseFloat(calc.x);
+        calc.sum = parseFloat(calc.x) - parseFloat(calc.y);
     }
     else if (calc.op === "*") {
-        calc.sum = parseFloat(calc.y) * parseFloat(calc.x);
+        calc.sum = parseFloat(calc.x) * parseFloat(calc.y);
     }
     else if (calc.op === "/") {
-        calc.sum = parseFloat(calc.y) / parseFloat(calc.x);
-        if (calc.x === "0" || calc.y === "0") {
+        calc.sum = parseFloat(calc.x) / parseFloat(calc.y);
+        if (calc.x === "0") {
             display.textContent = "can't do that"
+            blink();
+            setTimeout(blink, 10);
             return
         }
     }
-    calc.sum = parseFloat(calc.sum.toFixed(12));
-    calc.y = "";
+    calc.sum = parseFloat(calc.sum.toFixed(10));
     calc.x = calc.sum;
-    calc.op = calc.op;
-    display.textContent = calc.sum;
+    display.textContent = calc.x;
 }
 
-//Memory clear and erase functions
 function clearMemory() {
     calc.x = "";
     calc.y = "";
@@ -120,37 +117,38 @@ function erase() {
     }
 }
 
-//Keyboard input
-window.addEventListener("keydown", keyboardInput);
-window.addEventListener("keydown", dotManager);
-number.forEach(number => number.addEventListener("click", keyboardInput));
-
 function keyboardInput(e) {
-    switch (e.key){
+    switch (e.key) {
         case "c":
             clearMemory();
             break;
         case "Backspace":
             erase();
+            e.preventDefault();
+            e.stopPropagation();
             break;
-        case "Enter": case " ":
+        case "Enter": case " ": case "=":
             document.getElementById("Enter").click();
+            e.preventDefault();
+            e.stopPropagation();
             break;
         case "+": case "-": case "*": case "/":
             document.getElementById(`${e.key}`).click();
+            e.preventDefault();
+            e.stopPropagation();
             break;
         case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9": case "0": case ".":
-           document.getElementById(`${e.key}`).click();
+            document.getElementById(`${e.key}`).click();
             break;
         case "Shift":
             break;
-            }
+        default:
+            blink();
+            setTimeout(blink, 10);
+            return
     }
+}
 
-
-
-//Fix bug where "num, =, +, num, =" results in "NaN"
-
-//allow incremental op by pressing =
-
-//Squash any bugs
+function blink() {
+    display.classList.toggle("blinking");
+}
