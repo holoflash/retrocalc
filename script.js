d9fde871b682e2afdb39bed260c1185958966886
holoflash <mikepalacemusic@gmail.com>
2022-11-10 12:22:11 +0100

    Fix all known bugs

--- script.js
+++ script.js
@@ -22,6 +22,11 @@ window.addEventListener("keydown", keyboardInput);
 window.addEventListener("keydown", dotManager);
 
 function userNumberInput() {
+    if (calc.x.length >= 16 || calc.y.length >= 16) {
+        blink();
+        setTimeout(blink, 10);
+        return;
+    }
     if (calc.x !== "" && calc.op !== "") {
         calc.y = calc.y.concat(`${this.getAttribute("number")}`);
         display.textContent = calc.y;
@@ -30,47 +35,66 @@ function userNumberInput() {
         calc.x = calc.x.concat(`${this.getAttribute("number")}`);
         display.textContent = calc.x;
     }
-    if (calc.x.length > 15 || calc.x.length > 15) {
-        blink();
-        setTimeout(blink, 10);
-        return
-    }
+    console.log("number " + calc.x + calc.op +  calc.y + " = "+ calc.sum)
 }
 
 function dotManager() {
     const regexDotStart = /^\./g;
-    let dotStart = regexDotStart.test(calc.x);
-    if (dotStart === true) {
+    let dotStartx = regexDotStart.test(calc.x);
+    let dotStarty = regexDotStart.test(calc.y);
+    if (dotStartx === true) {
         display.innerText = "0."
         calc.x = "0."
         blink();
         setTimeout(blink, 10);
-        return
+    }
+    if (dotStarty === true) {
+        display.innerText = "0."
+        calc.y = "0."
+        blink();
+        setTimeout(blink, 10);
     }
     const regexMultiDot = /(\..*){2,}/;
-    let multiDot = regexMultiDot.test(calc.x);
-    if (multiDot === true) {
+    let multiDotx = regexMultiDot.test(calc.x);
+    let multiDoty = regexMultiDot.test(calc.y);
+    if (multiDotx === true) {
         erase();
+        blink();
+        setTimeout(blink, 10);
+    }
+    if (multiDoty === true) {
+        erase();
+        blink();
+        setTimeout(blink, 10);
     }
 }
 
 function userOperatorInput() {
+    blink();
+    setTimeout(blink, 10);
+    if (calc.sum !==""){
+        calc.y = "";
+        calc.op = this.getAttribute("name");
+    }
     if (calc.x !== "" && calc.op === "") {
         calc.op = this.getAttribute("name");
     }
-    if (calc.x !== "" && calc.y !== "" & calc.op !== "") {
+    if (calc.x !== "" && calc.y !== "" && calc.op !== "") {
+        calculate();
         calc.op = this.getAttribute("name");
         calc.y = "";
     }
-    if(calc.x ===""){
+    if (calc.x === "") {
         blink();
         setTimeout(blink, 10);
         return
     }
+    console.log("operator " + calc.x + calc.op +  calc.y + " = "+ calc.sum)
 }
 
 function calculate() {
-    if (calc.op === "" || calc.y ==="") {
+   
+    if (calc.op === "" || calc.y === "") {
         blink();
         setTimeout(blink, 10);
         return
@@ -96,6 +120,7 @@ function calculate() {
     calc.sum = parseFloat(calc.sum.toFixed(10));
     calc.x = calc.sum;
     display.textContent = calc.x;
+    console.log("result " + calc.x + calc.op +  calc.y + " = "+ calc.sum)
 }
 
 function clearMemory() {
@@ -104,16 +129,31 @@ function clearMemory() {
     calc.op = "";
     calc.sum = "";
     display.textContent = "0";
+    blink();
+    setTimeout(blink, 10);
 }
 
 function erase() {
-    calc.x = calc.x.slice(0, -1);
-    display.textContent = calc.x;
-    if (calc.x === "") {
-        display.textContent = "0";
+    blink();
+    setTimeout(blink, 10);
+    if (calc.y === "" && calc.op ==="" && calc.sum ==="") {
+        calc.x = calc.x.slice(0, -1);
+        display.textContent = calc.x;
+        if (calc.x === "") {
+            display.textContent = "0";
+            blink();
+            setTimeout(blink, 10);
+        }
     }
-    if (calc.x === "") {
+
+    if (calc.y !=="" && calc.op !=="" && calc.sum ==="") {
         calc.y = calc.y.slice(0, -1);
+        display.textContent = calc.y;
+        if (calc.y === "") {
+            display.textContent = "0";
+            blink();
+            setTimeout(blink, 10);
+        }
     }
 }
 
@@ -134,8 +174,6 @@ function keyboardInput(e) {
             break;
         case "+": case "-": case "*": case "/":
             document.getElementById(`${e.key}`).click();
-            e.preventDefault();
-            e.stopPropagation();
             break;
         case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9": case "0": case ".":
             document.getElementById(`${e.key}`).click();
